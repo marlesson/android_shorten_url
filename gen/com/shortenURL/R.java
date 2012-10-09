b0c40e16bed0672920ed93c900210d661af4973d
Marlesson <marlessonsa@gmail.com>
2012-10-08 22:28:01 -0300

    listener teste

--- gen/com/shortenURL/R.java
+++ gen/com/shortenURL/R.java
@@ -15,8 +15,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int btn_copy=0x7f070008;
         public static final int btn_shortener=0x7f070001;
-        public static final int menu_settings=0x7f070008;
+        public static final int menu_settings=0x7f070009;
         public static final int r_bitly=0x7f070005;
         public static final int r_googl=0x7f070006;
         public static final int r_migreme=0x7f070003;
--- src/com/shortenURL/MainActivity.java
+++ src/com/shortenURL/MainActivity.java
@@ -2,14 +2,65 @@ package com.shortenURL;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
 
+	private Button btn_shortener;
+	private EditText txt_url;
+	private Button btn_copy;
+	private RadioGroup rg_server;
+	private EditText txt_url_result;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        btn_shortener 	= (Button) 	 findViewById(R.id.btn_shortener);
+        txt_url 		= (EditText) findViewById(R.id.txt_url);
+        btn_copy        = (Button)   findViewById(R.id.btn_copy);
+        rg_server 		= (RadioGroup)   findViewById(R.id.rg_server);
+        txt_url_result  = (EditText)   findViewById(R.id.txt_url_result);
+        
+        btn_shortener.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				switch (rg_server.getCheckedRadioButtonId()) {
+			    case R.id.r_migreme:
+					txt_url_result.setText("Migreme");			       
+			        break;
+			    case R.id.r_tiny:
+			    	txt_url_result.setText("tiny");
+			        break;
+			    case R.id.r_bitly:
+			    	txt_url_result.setText("bitly");
+			        break;
+			    case R.id.r_googl:
+			    	txt_url_result.setText("Google");
+			        break;
+			}				
+
+			}
+
+		});
+        
+        btn_copy.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		AlertDialog.Builder mensagem = new
+				AlertDialog.Builder(MainActivity.this);
+				mensagem.setTitle("Message");
+				mensagem.setMessage("Ctrl+C");
+				mensagem.setNeutralButton("OK", null);
+				mensagem.show();
+			}
+        });          
     }
 
     @Override
